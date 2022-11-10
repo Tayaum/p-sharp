013787f2f9514709a605c2ad976c3380a166d422
Tyu13 <113293857+Tayaum@users.noreply.github.com>
2022-11-10 13:04:35 +0000

    Initial commit

---
+++ error.py
@@ -0,0 +1,6 @@
+import sys
+
+class Error():
+    def __init__(self, error) -> None:
+        print(error)
+        sys.exit()
---
+++ interpreter.py
@@ -0,0 +1,98 @@
+import os.path
+import subprocess
+import sys
+import shutil
+
+#PyPy
+pyInstallerInstalled = True
+
+try:
+    import PyInstaller.__main__ as PyInstaller
+except ImportError:
+    pyInstallerInstalled = False
+
+#Custom
+from parse import Parser
+from error import Error
+
+version = "0.0.1"
+
+class Compiler:
+    def compile(self, code : str, file) -> None:
+        subprocess.call(["python", file.replace(".ps", "")])
+        os.remove(file.replace(".ps", ""))
+def GetCode(filePath) -> str:
+    if os.path.isfile(filePath):
+        with open(filePath, 'r') as file:
+            return file.read()
+    else:
+        Error("Input file not found")
+
+def HandleArgs() -> None:
+    if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+        Error('''
+        Command line arguments:
+        --help -h: Prints this message
+        --version -b: Prints the version of the interpreter
+        --intrepret -i [file]: Runs the interpreter on the file specified
+        --transpile -t [file] [address]: Converts the file specified into python code and saves it to the address specified
+        --compile -c [file] [address]: Compiles the file specified into an executable and saves it to the address specified
+            ''')
+    elif sys.argv[1] == "--interpret" or sys.argv[1] == "-i":
+        if len(sys.argv) < 3:
+            Error("Invalid number of arguments")
+        else:
+            if os.path.isfile(sys.argv[2]):
+                parser = Parser(GetCode((sys.argv[2])), sys.argv[2])
+                compiler = Compiler()
+                compiler.compile(parser.code, sys.argv[2])
+            else:
+                Error("File not found")
+    elif os.path.isfile(sys.argv[1]):
+        parser = Parser(GetCode(sys.argv[1]))
+        compiler = compiler()
+        compiler.compile(parser.code)
+    elif sys.argv[1] == "--transpile" or sys.argv[1] == "-t":
+        if len(sys.argv) < 4:
+            Error("Invalid number of arguments")
+        else:
+            if os.path.isfile(sys.argv[2]):
+                parser = Parser(GetCode((sys.argv[2])))
+                with open(sys.argv[3], "w") as f:
+                    f.write(parser.code)
+            else:
+                Error("Input file not found")
+    elif sys.argv[1] == "--compile" or sys.argv[1] == "-c":
+        if pyInstallerInstalled == False:
+            Error("PyInstaller is not installed \n Please run \"pip install PyInstaller\"")
+        if len(sys.argv) < 4:
+            Error("Invalid number of arguments")
+        else:
+            if os.path.isfile(sys.argv[2]):
+                parser = Parser(GetCode((sys.argv[2])))
+                fileName = sys.argv[3].split(".")[0]
+                with open(fileName + ".py", "w") as f:
+                    f.write(parser.code)
+                if (os.path.isfile(fileName + ".exe")):
+                    os.remove(fileName + ".exe")
+                subprocess.call(["PyInstaller", fileName, "--onefile"])
+                os.rename("dist/{}".format(fileName+".exe"), "./"+sys.argv[3])
+                os.remove(fileName)
+                os.remove(fileName + ".spec")
+                shutil.rmtree("build")
+                shutil.rmtree("dist")
+            else:
+                Error("File not found")
+    else:
+        Error("Invalid argument")
+
+    if (os.path.isfile(sys.argv[1])):
+        os.remove(sys.argv[1])
+
+def CheckArgs() -> str:
+    if len(sys.argv) < 2:
+        Error("Invalid number of arguments")
+    HandleArgs()
+    
+if __name__ == "__main__":
+    CheckArgs()
